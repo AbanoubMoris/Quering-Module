4520f07c868bce9c651b06e266da553689822da8
RamezNabil <38498534+RamezNabil@users.noreply.github.com>
2019-04-22 22:10:36 +0200

    k

---
+++ Quering module/Quering module/Department.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+using System.IO;
+
+namespace Quering_module
+{
+    public class Department
+    {
+        public string ID { get;  set; }
+        public string Name { get; set; }
+    }
+
+    public class DepartmentsList
+    {
+
+        public List<Department> ReadDepartments()
+        {
+            List<Department> departments = new List<Department>();
+
+            XmlSerializer serializer = new XmlSerializer(departments.GetType());
+
+            StreamReader reader = new StreamReader("Departments.xml");
+            departments = (List<Department>)serializer.Deserialize(reader);
+            reader.Close();
+            return departments;
+
+        }
+    }
+}
