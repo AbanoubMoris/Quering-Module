607bca088f613ff638e5ac942b3206b4bb4f2f1c
abanoub abanoub <abanoub.goker@gmail.com>
2019-04-22 16:29:43 +0200

    1

---
+++ Quering module/Quering module/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Quering_module
+{
+    public partial class Form1 : Form
+    {
+
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            TablePnl.Visible = true;
+            ResPnl.Visible = false;
+            QueryPnl.Visible = false;
+
+
+        }
+
+        private void Button2_Click(object sender, EventArgs e)
+        {
+            TablePnl.Visible = false;
+            ResPnl.Visible = false;
+            QueryPnl.Visible = true;
+        }
+
+        private void Button3_Click(object sender, EventArgs e)
+        {
+            TablePnl.Visible = false;
+            ResPnl.Visible = true;
+            QueryPnl.Visible = false;
+        }
+    }
+}
---
+++ Quering module/Quering module/product.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Quering_module
+{
+    [Serializable]
+    class product
+    {
+        public int Id;
+        public String Name;
+        public int price;
+
+        public List<List<string>> prod = new List<List<string>>();
+        public void ReadData()
+        {
+            XmlSerializer xsSubmit = new XmlSerializer(prod.GetType());
+        }
+    }
+}
