370e777982341ff3777b5c47b0cac83640761c1d
AbanoOoub <40705922+AbanoOoub@users.noreply.github.com>
2019-04-22 18:20:51 +0200

    00

--- Quering module/Quering module/product.cs
+++ Quering module/Quering module/product.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 using System.Xml.Serialization;
 
 namespace Quering_module
@@ -13,11 +14,15 @@ namespace Quering_module
         public int Id;
         public String Name;
         public int price;
-
-        public List<product> prod = new List<product>();
+        public List<product> prod = new List<product>;
+           
         public void ReadData()
         {
             XmlSerializer xsSubmit = new XmlSerializer(prod.GetType());
+            FileStream fs = new FileStream("product.xml", FileMode.Open);
+            prod = (List<product>)xsSubmit.Deserialize(fs);
+            fs.Close();
+           
         }
     }
 }
